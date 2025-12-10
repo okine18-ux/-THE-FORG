3882af28e32f681612ad1c5e613f08ef3d4c8d75
okine18-ux <cpagamer18@gmail.com>
2025-12-10 22:25:41 +0100

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,16 +1,16 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This check prevents the app from crashing if the 'root' element is not in the HTML.
+// This allows the static index.html to be displayed without errors from the React setup.
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
